5e459b46cb264255b1c8b8153eb8dabf6c7c5380
Juan Toro <juan.toro@globant.com>
2017-12-28 15:56:56 -0500

    Change project name

---
+++ src/main/java/com/tclab/repovtx/FeatureVerticle.java
@@ -0,0 +1,16 @@
+package com.tclab.repovtx;
+
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class FeatureVerticle extends AbstractVerticle {
+
+	@Override
+	public void start(Future<Void> fut) {
+
+		
+	}
+
+	
+}
